f089932219ca42eb8b6b6a917586e135f3df3776
Mamadou Ndiaye <epinotes@gmail.com>
2019-03-04 18:41:14 -0800

    Create first_valid_f.r
    find the column that first matches. create a binary of 1 if match 0 otherwise

---
+++ first_valid_f.r
@@ -0,0 +1,10 @@
+first_valid_f <- function(data, colvec, pattern){
+  #colvec: the columns of interest
+  # pattern: the regex pattern to match
+  colvec = enquo(colvec)
+  f0 <- function(x) grepl(pattern = pattern, x, ignore.case = T, perl = T)
+  f1 <- function(x) detect_index(x, f0)
+  data %>% select(!!colvec) %>% 
+    map_dfr(as.character) %>% transpose %>% 
+    map_int(f1) %>% sign()
+}
