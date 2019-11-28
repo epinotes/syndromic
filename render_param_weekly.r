b63335592130a09b617ce1a5f7d2b703364971d8
Mamadou Ndiaye <epinotes@gmail.com>
2019-11-28 09:59:57 -0800

    Delete render_param_weekly.r

--- render_param_weekly.r
+++
@@ -1,9 +0,0 @@
-geo <- "County_name" 
-
-rmarkdown::render(
-  input = "weekly_county_specific.Rmd", 
-  params = list(geography = geo)
-)
-
-fs::file_copy("weekly_county_specific.html", 
-              glue::glue("weekly_county_{geo}.html"), overwrite = TRUE)
